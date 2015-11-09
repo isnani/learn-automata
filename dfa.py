ffb3ab70daa3ead00bc354c486819b75c1d2f1a4
isnani <isnanipramusinto@gmail.com>
2015-11-09 07:01:47 +0100

    edited to follow PEP8

--- dfa.py
+++ dfa.py
@@ -1,26 +1,25 @@
-#Deterministic Finite Automata
+# Deterministic Finite Automata
 
 class Dfa(object):
     
     def __init__(self, states, alphabet, delta, start, final):
-        #states, alphabet, final are set.
-        #delta is set of tuple
-        #start is string
+        # states, alphabet, final are set.
+        # delta is set of tuple
+        # start is string
 
         self.states = states
         self.alphabet = alphabet
         self.delta = delta
         self.start = start
         self.final = final
-    
 
-    def isDfa(self):
+    def is_dfa(self):
         '''
         by definition somewhere, dfa is characterized by its transition function, where each state only has outcoming
         transition reading exactly one of every symbol in alphabet. Even the transition goes to dead state.
         Considering that dead state doesn't necessarily appear.
         '''
-        #later this funtion should be set to private, and is used to ensure validity of the instance of this class
+        # later this function should be set to private, and is used to ensure validity of the instance of this class
 
         count = 0
         num_symbols = len(self.alphabet)
@@ -36,8 +35,7 @@ class Dfa(object):
         else:
             return True
 
-
-    def printOut(self):
+    def print_out(self):
         '''
         to display the component of automata   
         '''
@@ -63,14 +61,13 @@ class Dfa(object):
         print ""
 
         print "Transition = "
-        for d in self.delta: #it was OK even when I forgot to ignore keyword self. why?
+        for d in self.delta:  # it was OK even when I forgot to ignore keyword self. why?
             print "(",
             for el in d:
                 print el,
             print ")"
 
-
-    def deltaFunction(self, state, symbol):
+    def delta_function(self, state, symbol):
         '''
         what state is now when you are from a state and reading a symbol.
         '''
@@ -82,7 +79,7 @@ class Dfa(object):
             return 's' 
 
 
-    def isAccepted(self, word):
+    def is_accepted(self, word):
         '''
         to check whether a given word is recognized (accepted) by the language or not. Recognized means
         the word is a member of language formed by the automata
@@ -90,14 +87,14 @@ class Dfa(object):
         
         current_state = self.start
         for symbol in range(0,len(word)):
-            next_state = self.deltaFunction(current_state, word[symbol])
+            next_state = self.delta_function(current_state, word[symbol])
             if next_state == 's':
                 return False
             else:
-               current_state = next_state
-               symbol+=1
+                current_state = next_state
+                symbol+=1
         else:
-            for q in final:
+            for q in self.final:
                 if current_state == q:
                     return True
                     break
@@ -115,15 +112,15 @@ class Dfa(object):
         return Dfa(self.states, self.alphabet, self.delta, self.start, final)
         
     def intersection(self, automata):
-        return self.__binOp(self, automata, 'and')
+        return self.__bin_op(self, automata, 'and')
 
     def union(self, automata):
-        return self.__binOp(self, automata, 'or')
+        return self.__bin_op(self, automata, 'or')
 
     def symdifference(self, automata):
-        return self.__binOp(self, automata, 'xor')
+        return self.__bin_op(self, automata, 'xor')
 
-    def __binOp(self, a1, a2, op):
+    def __bin_op(self, a1, a2, op):
         '''
         this function is used by union, intersection and symmetric different operations. That because they differ only on
         how to determine the final states.
@@ -134,9 +131,10 @@ class Dfa(object):
         final = set([])
         start = (a1.start, a2.start)
         delta = set([])
-        worklist = set((start,)) ##interesting here is how to initialize a set of tuples with one member (a single tuple). be careful! 
-        while worklist != set([]):
-            pick = worklist.pop()
+        working_list = set((start,))
+        # interesting above is how to initialize a set of tuples with one member (a single tuple). be careful!
+        while working_list != set([]):
+            pick = working_list.pop()
             states.add(pick)
             if op == 'and':
                 if pick[0] in a1.final and pick[1] in a2.final:
@@ -149,15 +147,14 @@ class Dfa(object):
                     final.add(pick)
 
             for symbol in alphabet:
-                q1 = a1.deltaFunction(pick[0], symbol)
-                q2 = a2.deltaFunction(pick[1], symbol)
+                q1 = a1.delta_function(pick[0], symbol)
+                q2 = a2.delta_function(pick[1], symbol)
                 if (q1,q2) not in states:
-                    worklist.add((q1,q2))
+                    working_list.add((q1,q2))
                 delta.add((pick, symbol, (q1,q2)))
         return Dfa(states, alphabet, delta, start, final)
 
-
-    def isUniversal(self):
+    def is_universal(self):
         '''
         dfa is universal if all states are final state, excluding unreachable states. as for dead state?? should be yes!
         then it need to check if dfa has 'complete' transition.
@@ -165,85 +162,85 @@ class Dfa(object):
         
         reachable = set([])
         for state in self.states:
-            if self.isReachable(state):
+            if self.is_reachable(state):
                 reachable.add(state)
         if reachable.difference(self.final) == set([]):
             return True
         else:
             return False
 
-    def isEmpty(self):
+    def is_empty(self):
         '''
         dfa is empty if there is no final state, excluding unreachable states.
-        doens't need to check the dead state. the dead state doesn't appear means it is not a final state.
+        doesn't need to check the dead state. the dead state doesn't appear means it is not a final state.
         '''
         
         if self.final == set([]):
             return True
         else:
             for state in self.final:
-                if self.isReachable(state):
+                if self.is_reachable(state):
                     return False
                 else:
                     continue
             else:
                 return True
 
-    def isReachable(self, state):
+    def is_reachable(self, state):
         '''
         reachable state means that such state can be reached from start state by running some transition
         '''
         
-        worklist = set([state])
-        #print worklist
+        working_list = set([state])
+        # print working list
         if state == self.start:
             return True
             
-        while worklist != ([]):
+        while working_list != ([]):
             previous_states = set([])
-            current_state = worklist.pop()
-            #print current_state
+            current_state = working_list.pop()
+            # print current_state
             for symbol in self.alphabet:
-                temp = set(states for states in self.states if self.deltaFunction(states, symbol) == current_state)
+                temp = set(states for states in self.states if self.delta_function(states, symbol) == current_state)
                 previous_states = previous_states.union(temp)
             previous_states.discard('s')
-            #print "previous states to", current_state, "=", previous_states
+            # print "previous states to", current_state, "=", previous_states
             if previous_states == set([]) or previous_states == set([state]):
                 return False
             else:
-                worklist = worklist.union(previous_states)
-                #print "all states reach", state, "=", worklist
-                worklist.discard(current_state)
-                if self.start in worklist:
+                working_list = working_list.union(previous_states)
+                # print "all states reach", state, "=", working_list
+                working_list.discard(current_state)
+                if self.start in working_list:
                     return True
-                
 
-    def minimizeByHopcroft(self):
+    def minimize_by_hopcroft(self):
         '''
         Hopcroft algorithm to minimize dfa
         '''
         
         final = self.final
-        notfinal = self.states.difference(self.final)
-        partition = {tuple(final), tuple(notfinal)} ##set of tuples, ideally set of sets but it is not allowed
-        ##initializing worklist
-        if len(final) <= len(notfinal):
-            worklist = set((tuple(final),y) for y in self.alphabet) ##{((),''),((),''),...} --> use .pop to empty the set
-            print "initial worklist= ",worklist
+        not_final = self.states.difference(self.final)
+        partition = {tuple(final), tuple(not_final)}  # ->set of tuples, ideally set of sets but it is not allowed
+        # ->initializing working_list
+        if len(final) <= len(not_final):
+            # ->{((),''),((),''),..} --> use .pop to empty the set
+            working_list = set((tuple(final),y) for y in self.alphabet)
+            print "initial working_list= ",working_list
             
         else:
-            worklist = set((tuple(notfinal),y) for y in self.alphabet)
-            print "initial worklist= ",worklist
+            working_list = set((tuple(not_final),y) for y in self.alphabet)
+            print "initial working_list= ",working_list
 
         print "----------------------------------------------"
 
-        while worklist != set([]):
-            splitter = worklist.pop()
+        while working_list != set([]):
+            splitter = working_list.pop()
             print "partition= ",partition
             print "splitter= ",splitter
             split_state = self.__split(splitter, partition)
             print "split_state =  ", split_state
-            worklist.discard(splitter) ##it raised KeyError when using .remove(el)
+            working_list.discard(splitter)  # ->it raised KeyError when using .remove(el)
             
             partition_copy = partition.copy()
                     
@@ -258,26 +255,26 @@ class Dfa(object):
                 print "one_part = ", one_part
                 print "other_part = ", other_part
                         
-                ##update partition, if one apart is affected
+                # #update partition, if one apart is affected
                 if one_part != () and other_part != ():
                     partition.discard(apart)
                     partition.add(one_part)
                     partition.add(other_part)
-                    ##update worklist
-                    
+
+                    # #update working_list
                     for symbol in self.alphabet:
-                        if (apart, symbol) in worklist:
-                            worklist.discard((apart,symbol))
-                            worklist.add((one_part,symbol))
-                            worklist.add((other_part,symbol))
+                        if (apart, symbol) in working_list:
+                            working_list.discard((apart,symbol))
+                            working_list.add((one_part,symbol))
+                            working_list.add((other_part,symbol))
                         else:
-                            worklist.add((one_part,symbol))
+                            working_list.add((one_part,symbol))
                     
             print "partition after once split = ", partition
-            print "worklist after once split = ", worklist
+            print "working_list after once split = ", working_list
             print "----------------------------------------------"
 
-        ##establish the resulted minimum automata
+        # #establish the resulted minimum automata
         new_alphabet = self.alphabet
         new_states = set([])
         new_start = self.start
@@ -306,21 +303,20 @@ class Dfa(object):
                 new_states.add(state)
 
         return Dfa(new_states, new_alphabet, new_delta, new_start, new_final)
-         
-        
+
     def __split(self, splitter, partition):
         '''
         this function is used by hopcroft algorithm, that is introducing the function to split partition,
         given both set of states and symbol incoming to them.
         '''
         
-        split_state = () #tuple
-        for apart in partition: #apart is tuple, partition is set
-            split_state += tuple(x for x in apart if self.deltaFunction(x,splitter[1]) in splitter[0])
+        split_state = ()  # tuple
+        for apart in partition:  # apart is tuple, partition is set
+            split_state += tuple(x for x in apart if self.delta_function(x,splitter[1]) in splitter[0])
         return split_state
 
     
-    def minimizeByMoore(self):
+    def minimize_by_moore(self):
         '''
         the idea is using complete graph (states as nodes) as starting step. Then mark all edges between final and
         non-final nodes. As long as there exist marked edges, repeat the following step. Choose (arbitrarily) a marked
@@ -335,21 +331,25 @@ class Dfa(object):
         print complete_graph
         
         final = self.final
-        notfinal = self.states.difference(self.final) 
-        marked = set(tup for tup in complete_graph if tup[0] in final and tup[1] in notfinal or tup[0] in notfinal and tup[1] in final)
+        not_final = self.states.difference(self.final)
+        marked = set(tup for tup in complete_graph if tup[0] in final and tup[1] in not_final or tup[0] in not_final and
+                     tup[1] in final)
         unmarked = complete_graph.difference(marked)
         unmarked_copy = unmarked.copy()
         while marked != set([]):
             a_marked = marked.pop()
             for tup in unmarked_copy:
                 for symbol in self.alphabet:
-                    if self.deltaFunction(tup[0], symbol) == a_marked[0] and self.deltaFunction(tup[1], symbol) == a_marked[1] or self.deltaFunction(tup[0], symbol) == a_marked[1] and self.deltaFunction(tup[1], symbol) == a_marked[0]:
+                    if self.delta_function(tup[0], symbol) == a_marked[0] and \
+                                    self.delta_function(tup[1], symbol) == a_marked[1] or \
+                                            self.delta_function(tup[0], symbol) == a_marked[1] and \
+                                            self.delta_function(tup[1], symbol) == a_marked[0]:
                         marked.discard(a_marked)
                         marked.add(tup)
                         unmarked.discard(tup)
                         break
 
-        #establish new dfa
+        # establish new dfa
         new_alphabet = self.alphabet
         new_states = self.states
         new_delta = self.delta
@@ -360,7 +360,7 @@ class Dfa(object):
         else:
             temp = set([])
             unmarked_copy = unmarked.copy()
-            for tup in unmarked: #unmarked might contain more than one edges
+            for tup in unmarked:  # unmarked might contain more than one edges
                 unmarked_copy.discard(tup)
                 for tup2 in unmarked_copy:
                     if tup2[0] in tup:
@@ -372,11 +372,10 @@ class Dfa(object):
                         if tup2[1] in tup:
                             temp.add(tup + tup2[0])
                         else:
-                            temp.add(tup) #may be true
+                            temp.add(tup)  # may be true
 
-                        
             if new_start in temp:
-                new_start = new_start+"*"
+                new_start =+"*"
             if new_final.intersection(temp) != set([]):
                 temp2 = new_final.intersection(temp)
                 new_final.difference_update(temp2)
@@ -387,27 +386,24 @@ class Dfa(object):
             new_states.add(new_start)
             new_states = new_states.union(new_final)
 
-            #next: new delta
+            # next: new delta
             new_delta_copy = new_delta.copy()
             for tup in new_delta_copy:
                 if tup[0] or tup[2] in temp:
                     pass
             return Dfa(new_states, new_alphabet, new_delta, new_start, new_final)
 
-
-    def minimizeByBrzozowski(self):
+    def minimize_by_brzozowski(self):
         pass
-
-
         
-    def converttoregex(self):
+    def convert_to_regex(self):
         '''
         our regular expression will only need symbols in alphabet, operator +, *,concatenation that has no shape,
         and parentheses to against priority.(* > . > +)
         '''
 
         regex = ''
-        worklist = set([])
+        working_list = set([])
         for tup in self.delta:
             if tup[0] == tup[2]:
                 regex += tup[1], '*'
